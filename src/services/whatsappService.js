1fe73ec121551985c8f038e097d2ca763a699fe5
fattakbar <akbarfattahul22@gmail.com>
2025-02-02 18:43:01 +0700

    fix message

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -32,7 +32,14 @@ const sendOtp = async (phone, otp) => {
             throw new Error("Nomor tidak valid atau tidak terdaftar di WhatsApp");
         }
 
-        await client.sendMessage(numberId._serialized, `Kode OTP Anda: ${otp}`);
+        const expiryTime = new Date(Date.now() + 5 * 60 * 1000);
+        const options = { timeZone: 'Asia/Jakarta', hour: '2-digit', minute: '2-digit', hour12: false };
+        const formattedExpiryTime = expiryTime.toLocaleTimeString('id-ID', options);
+
+        await client.sendMessage(
+            numberId._serialized,
+            `*SILETON KABUPATEN AGAM*\n\nTerima kasih telah mendaftar di aplikasi SILETON. Berikut adalah kode verifikasi OTP Anda:\n\n*${otp}*\n\nKode ini hanya berlaku selama 5 menit. Kode akan kedaluwarsa pada pukul ${formattedExpiryTime} WIB.\n\n⚠️ *PERHATIAN:* Jangan beritahukan kode ini kepada siapa pun, termasuk pihak SILETON. Kami tidak akan pernah meminta kode OTP Anda.`
+        );
         return { success: true, message: "OTP terkirim" };
     } catch (error) {
         console.error("Gagal mengirim OTP:", error.message);
